7d67b0dbb32629d45c51e30fde745abc30daee7b
Ariyan1562 <rivuariyan01@gmail.com>
2025-04-14 01:54:38 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Flight {
+    int id;
+    String flightName, departure, destination;
+    int availableSeats;
+    double price;
+
+    public Flight(int id, String flightName, String departure, String destination, int availableSeats, double price) {
+        this.id = id;
+        this.flightName = flightName;
+        this.departure = departure;
+        this.destination = destination;
+        this.availableSeats = availableSeats;
+        this.price = price;
+    }
+}
+
+class User {
+    String username, password;
+    boolean isAdmin;
+
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
         FlightReservationSystem.main(args);
